d832794af71edfe7f7bce7038648a4d71a8a9316
Giselle Que <giselleque@gmail.com>
2020-07-26 16:58:02 +0800

    created new pages

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,21 @@ import React from 'react';
 import logo from './logo.svg';
 import styles from './App.scss';
 
+import Navbar from './components/Navbar';
+import {BrowserRouter, Route, Switch} from 'react-router-dom';
+import Home from './components/Home';
+import About from './components/About';
+
 function App() {
   return (
+    <BrowserRouter>
     <div className="App">
-      <header className="App-header">
+      <Navbar />
+      <Switch>
+        <Route exact path="/" component={Home}/>
+        <Route path="/about" component={About}/>
+      </Switch>
+      {/* <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
@@ -18,8 +29,9 @@ function App() {
         >
           Learn React
         </a>
-      </header>
+      </header> */}
     </div>
+    </BrowserRouter>
   );
 }
 
